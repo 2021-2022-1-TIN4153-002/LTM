2e6c582579c6a6cae1cb7a9792082b2dedf24e99
vvdung3508 <vvdung3508@gmail.com>
2021-09-18 01:13:37 -0700

    vvdung

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 
 int main(){
-  printf("WELCOME VSCODE! Toi la NVT");
+  printf("**** WELCOME VSCODE! Toi la NVT\n");
+  for (int i = 0; i < 5; ++ i) printf("---\n");
+  printf("\n*** EXIT APP ***\n");
   return 1;
 }
